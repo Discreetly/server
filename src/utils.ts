08f65ffe358812c8b40383ef09c85f93d6fb590f
Tanner Shaw <traumaaa@gmail.com>
2023-07-08 19:58:50 -0500

    changed id to groupId

--- src/utils.ts
+++ src/utils.ts
@@ -3,10 +3,10 @@ import { createClient } from 'redis';
 const redisClient = createClient();
 redisClient.connect();
 
-export const addIdentityToRoom = (id, IdentityCommitment) => {
+export const addIdentityToRoom = (groupId, IdentityCommitment) => {
   redisClient.get('rooms').then(res => {
     const data = JSON.parse(res);
-    const roomIndex = data[0].rooms.findIndex(room => room.id === id);
+    const roomIndex = data[0].rooms.findIndex(room => room.id === groupId);
     const roomToAdd = data[0].rooms[roomIndex];
     // console.log(roomToAdd.membership.identityCommitments.find(identity => identity === idc));
     if (!roomToAdd.membership.identityCommitments.find(identity => identity === IdentityCommitment)) {
