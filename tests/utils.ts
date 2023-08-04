5bc46cdb6b88cc8e1f877b3253dd202737073af6
AtHeartEngineer <atheartengineer@gmail.com>
2023-08-04 14:48:39 -0400

    fix return in test utils

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,9 +1,9 @@
 import { faker } from '@faker-js/faker';
 
-export function randBigint() {
+export function randBigint(): bigint {
   return faker.number.bigInt();
 }
 
-export function randomRoomName(min = 5, max = 20) {
-  faker.string.alphanumeric({ length: { min: min, max: max } });
+export function randomRoomName(min = 5, max = 20): string {
+  return faker.string.alphanumeric({ length: { min: min, max: max } });
 }
