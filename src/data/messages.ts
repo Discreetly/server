e371b1ef340a006cab7c6d4a6829a2cbc0509d6e
AtHeartEngineer <atheartengineer@gmail.com>
2023-08-15 17:43:07 -0400

    fix .catch + style

--- src/data/messages.ts
+++ src/data/messages.ts
@@ -1,8 +1,8 @@
-import { getRoomByID } from "./db";
-import { PrismaClient } from "@prisma/client";
-import { MessageI } from "discreetly-interfaces";
-import { shamirRecovery } from "../crypto/shamirRecovery";
-import { RLNFullProof } from "rlnjs";
+import { getRoomByID } from './db';
+import { PrismaClient } from '@prisma/client';
+import { MessageI } from 'discreetly-interfaces';
+import { shamirRecovery } from '../crypto/shamirRecovery';
+import { RLNFullProof } from 'rlnjs';
 
 const prisma = new PrismaClient();
 
@@ -39,7 +39,7 @@ export async function checkRLNCollision(
   roomId: string,
   message: MessageI
 ): Promise<CollisionCheckResult> {
-  return new Promise((res, rej) => {
+  return new Promise((res) => {
     prisma.rooms
       .findFirst({
         where: { roomId },
@@ -48,15 +48,15 @@ export async function checkRLNCollision(
             where: { epoch: String(message.epoch) },
             include: {
               messages: {
-                where: { messageId: message.messageId },
-              },
-            },
-          },
-        },
+                where: { messageId: message.messageId }
+              }
+            }
+          }
+        }
       })
       .then((oldMessage) => {
         if (!message.proof) {
-          throw new Error("Proof not provided");
+          throw new Error('Proof not provided');
         }
         if (!oldMessage) {
           res({ collision: false } as CollisionCheckResult);
@@ -64,23 +64,19 @@ export async function checkRLNCollision(
           const oldMessageProof = JSON.parse(
             oldMessage.epochs[0].messages[0].proof
           ) as RLNFullProof;
-          const oldMessagex2 = BigInt(
-            oldMessageProof.snarkProof.publicSignals.x
-          );
-          const oldMessagey2 = BigInt(
-            oldMessageProof.snarkProof.publicSignals.y
-          );
+          const oldMessagex2 = BigInt(oldMessageProof.snarkProof.publicSignals.x);
+          const oldMessagey2 = BigInt(oldMessageProof.snarkProof.publicSignals.y);
 
           let proof: RLNFullProof;
 
-          if (typeof message.proof === "string") {
+          if (typeof message.proof === 'string') {
             proof = JSON.parse(message.proof) as RLNFullProof;
           } else {
-            proof = message.proof as RLNFullProof;
+            proof = message.proof;
           }
           const [x1, y1] = [
             BigInt(proof.snarkProof.publicSignals.x),
-            BigInt(proof.snarkProof.publicSignals.y),
+            BigInt(proof.snarkProof.publicSignals.y)
           ];
           const [x2, y2] = [oldMessagex2, oldMessagey2];
 
@@ -89,20 +85,21 @@ export async function checkRLNCollision(
           res({
             collision: true,
             secret,
-            oldMessage: oldMessage.epochs[0].messages[0] as MessageI,
+            oldMessage: oldMessage.epochs[0].messages[0] as unknown as MessageI
           } as CollisionCheckResult);
         }
-      });
+      })
+      .catch((err) => console.error(err));
   });
 }
 
 function addMessageToRoom(roomId: string, message: MessageI): Promise<unknown> {
   if (!message.epoch) {
-    throw new Error("Epoch not provided");
+    throw new Error('Epoch not provided');
   }
   return prisma.rooms.update({
     where: {
-      roomId: roomId,
+      roomId: roomId
     },
     data: {
       epochs: {
@@ -110,46 +107,51 @@ function addMessageToRoom(roomId: string, message: MessageI): Promise<unknown> {
           epoch: String(message.epoch),
           messages: {
             create: {
-              message: message.message ? message.message.toString() : "",
-              messageId: message.messageId ? message.messageId.toString() : "",
+              message: message.message ? message.message.toString() : '',
+              messageId: message.messageId ? message.messageId.toString() : '',
               proof: JSON.stringify(message.proof),
-              roomId: roomId,
-            },
-          },
-        },
-      },
-    },
+              roomId: roomId
+            }
+          }
+        }
+      }
+    }
   });
 }
 
 export function createMessage(roomId: string, message: MessageI): boolean {
-  getRoomByID(roomId).then((room) => {
-    if (room) {
-      // Todo This should check that there is no duplicate messageId with in this room and epoch,
-      // if there is, we need to return an error and
-      // reconstruct the secret from both messages, and ban the user
-      checkRLNCollision(roomId, message)
-        .then((collisionResult) => {
-          if (!collisionResult.collision) {
-            addMessageToRoom(roomId, message)
-              .then((roomToUpdate) => {
-                console.log(roomToUpdate);
-                return true;
-              })
-              .catch((error) => {
-                console.error(`Error updating room: ${error}`);
-                return false;
-              });
-          } else {
-            console.log("Collision found");
+  getRoomByID(roomId)
+    .then((room) => {
+      if (room) {
+        // Todo This should check that there is no duplicate messageId with in this room and epoch,
+        // if there is, we need to return an error and
+        // reconstruct the secret from both messages, and ban the user
+        checkRLNCollision(roomId, message)
+          .then((collisionResult) => {
+            if (!collisionResult.collision) {
+              addMessageToRoom(roomId, message)
+                .then((roomToUpdate) => {
+                  console.log(roomToUpdate);
+                  return true;
+                })
+                .catch((error) => {
+                  console.error(`Error updating room: ${error}`);
+                  return false;
+                });
+            } else {
+              console.log('Collision found');
+              return false;
+            }
+          })
+          .catch((error) => {
+            console.error(`Error getting room: ${error}`);
             return false;
-          }
-        })
-        .catch((error) => {
-          console.error(`Error getting room: ${error}`);
-          return false;
-        });
-    }
-  });
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(`Error getting room: ${error}`);
+      return false;
+    });
   return false;
 }
