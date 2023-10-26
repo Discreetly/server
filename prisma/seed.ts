47fea181dc59a63544c947b11ce5b96f2de9e5f9
Tanner Shaw <traumaaa@gmail.com>
2023-10-26 15:58:22 -0500

    fix(seed): updating js docs for seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,11 +2,16 @@ import { createRoom } from '../src/data/db';
 import { createEthGroup } from '../src/data/db';
 import addresses from './addresses';
 async function main() {
-  //   @param name — The name of the room.
-  // @param rateLimit — The length of an epoch in milliseconds
-  // @param userMessageLimit — The message limit per user per epoch
-  // @param numClaimCodes — The number of claim codes to generate for the room.
-  // @param approxNumMockUsers — The approximate number of mock users to generate for the room.
+  /**
+  *   @param name — The name of the room.
+  * @param rateLimit — The length of an epoch in milliseconds
+  * @param userMessageLimit — The message limit per user per epoch
+  * @param numClaimCodes — The number of claim codes to generate for the room.
+  * @param approxNumMockUsers — The approximate number of mock users to generate for the room.
+  * @param type - The type of room. PUBLIC or PRIVATE
+  * @param adminIdentities - The identities of the admins of the room.
+  * @param roomId - The roomId of the room to be created.
+  */
   await createRoom('2 Second Room', 2000, 1, 0, 20, 'PUBLIC', [], '20155607622739913528708582707389798203895994292773716935431197669304746014398');
   await createRoom('10 Second Room', 10000, 3, 0, 20, 'PUBLIC', [], '15365950124115259122299397335353503712492707509718474633204755132763780105662');
   await createRoom('100 Second Room', 100000, 12, 0, 20, 'PUBLIC', [], '9140365210618339744130775644008692650778595188985750546820671632708469016277');
